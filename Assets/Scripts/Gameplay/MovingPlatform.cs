6470825bc755a0cd46b4f803a9931f2706097468
Daniel Lum <kanekoa@Daniels-MacBook-Pro.local>
2019-12-16 23:01:02 -0800

    New moving platform levels

--- Assets/Scripts/Gameplay/MovingPlatform.cs
+++ Assets/Scripts/Gameplay/MovingPlatform.cs
@@ -8,6 +8,7 @@ public class MovingPlatform : MonoBehaviour
     public Vector3 Offset;
     public float MoveDuration = 5;
     public float PauseDuration = 0;
+    public float InitialDelay = 0f;
 
     private Vector3 _originalPos;
     private bool _moveFlip;
@@ -17,7 +18,19 @@ public class MovingPlatform : MonoBehaviour
     {
         _originalPos = transform.position;
 
-        StartCoroutine(waitToMove());
+        StartCoroutine(initialDelay());
+    }
+
+    IEnumerator initialDelay()
+    {
+        yield return new WaitForSeconds(InitialDelay);
+        startMovement();
+    }
+
+    IEnumerator waitToMove()
+    {
+        yield return new WaitForSeconds(PauseDuration);
+        startMovement();
     }
 
     void startMovement()
@@ -34,11 +47,5 @@ public class MovingPlatform : MonoBehaviour
         });
 
         _moveFlip = !_moveFlip;
-    }
-
-    IEnumerator waitToMove()
-    {
-        yield return new WaitForSeconds(PauseDuration);
-        startMovement();
     }
 }
