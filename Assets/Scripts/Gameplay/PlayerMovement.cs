bbc21465315c77f72b7402762d5192d7ed913773
Daniel Lum <kanekoa@Daniels-MacBook-Pro.local>
2019-12-17 17:47:48 -0800

    Fix lagging movement

--- Assets/Scripts/Gameplay/PlayerMovement.cs
+++ Assets/Scripts/Gameplay/PlayerMovement.cs
@@ -5,31 +5,27 @@ using System;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [NonSerialized] CharacterController characterController;
+    public CharacterController characterController;
 
     public float speed = 6.0f;
     public float gravity = 20.0f;
     public float turnSpeed = 10f;
 
-    private Vector3 moveDirection = Vector3.zero;
+    private Vector3 _moveDirection = Vector3.zero;
     private bool _isDead;
+    private bool _isGrounded;
     private int _deadzoneMask;
     private int _groundMask;
+    private float _yVelocity;
 
     void Awake()
     {
         GlobalEvents.LoseLevel.AddListener(handleLoseLevel);
-        GlobalEvents.StartLevel.AddListener(handleStartLevel);
         GlobalEvents.WinLevel.AddListener(handleWinLevel);
 
         _deadzoneMask = 1 << Layers.DEADZONE;
         _groundMask = 1 << Layers.GROUND;
         _isDead = false;
-    }
-
-    void Start()
-    {
-        characterController = GetComponent<CharacterController>();        
     }
 
     private void handleWinLevel()
@@ -42,52 +38,38 @@ public class PlayerMovement : MonoBehaviour
         _isDead = true;
     }
 
-    private void handleStartLevel()
-    {
-        StartCoroutine(respawn());      
-    }
-
-    IEnumerator respawn()
-    {
-        yield return new WaitForSeconds(0.25f);
-        _isDead = false;
-    }
-
     void Update()
     {
         setPlatform();
+        setDead();
+        move();        
+    }
 
+    void move()
+    {
         if (!_isDead)
-        {            
-            _isDead = Physics.Raycast(transform.position, Vector3.down, 0.25f, _deadzoneMask);
-            if (_isDead)
-            {
-                GlobalEvents.LoseLevel.Invoke();
-            }
-        }
-
-        if(!_isDead)
         {
+            _moveDirection = getMoveVector();
+            handleRotation(_moveDirection);
+            _moveDirection *= speed;
+            if (_moveDirection.magnitude > speed)
+            {
+                _moveDirection = _moveDirection.normalized * speed;
+            }
+
             if (characterController.isGrounded)
             {
-                moveDirection = getMoveVector();
-
-                handleRotation(moveDirection);
-
-                moveDirection *= speed;
-                if (moveDirection.magnitude > speed)
-                {
-                    moveDirection = moveDirection.normalized * speed;
-                }
+                _yVelocity = 0;
             }
             else
             {
-                moveDirection.y -= gravity * Time.deltaTime;
+                _yVelocity -= gravity * Time.deltaTime;
+                _moveDirection.y = _yVelocity;
             }
 
-            if (moveDirection.sqrMagnitude > Mathf.Epsilon)
+            if (_moveDirection.sqrMagnitude > Mathf.Epsilon)
             {
-                characterController.Move(moveDirection * Time.deltaTime);
+                characterController.Move(_moveDirection * Time.deltaTime);
             }
         }
     }
@@ -119,10 +101,24 @@ public class PlayerMovement : MonoBehaviour
         return moveVector;
     }
 
+    private void setDead()
+    {
+        if (!_isDead)
+        {
+            _isDead = Physics.Raycast(transform.position, Vector3.down, 0.25f, _deadzoneMask);
+            if (_isDead)
+            {
+                transform.parent = null;
+                GlobalEvents.LoseLevel.Invoke();
+            }
+        }
+    }
+
     private void setPlatform()
     {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, 1, _groundMask))
+        _isGrounded = Physics.Raycast(transform.position, Vector3.down, out hit, 1, _groundMask);
+        if(_isGrounded)
         {
             if (transform.parent != hit.transform.parent)
             {
