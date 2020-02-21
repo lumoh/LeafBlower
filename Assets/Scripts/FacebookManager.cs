7cd924808f4973d57363d984c41d24d3d8aac0d7
Daniel Lum <kanekoa@Daniels-MacBook-Pro.local>
2020-02-20 21:36:05 -0800

    Facebook SDK install and running on android device

---
+++ Assets/Scripts/FacebookManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Facebook.Unity;
+using UnityEngine;
+
+public class FacebookManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (FB.IsInitialized)
+        {
+            FB.ActivateApp();
+        }
+        else
+        {
+            //Handle FB.Init
+            FB.Init(() => {
+                FB.ActivateApp();
+            });
+        }
+    }
+
+    // Unity will call OnApplicationPause(false) when an app is resumed
+    // from the background
+    void OnApplicationPause(bool pauseStatus)
+    {
+        // Check the pauseStatus to see if we are in the foreground
+        // or background
+        if (!pauseStatus)
+        {
+            //app resume
+            if (FB.IsInitialized)
+            {
+                FB.ActivateApp();
+            }
+            else
+            {
+                //Handle FB.Init
+                FB.Init(() => {
+                    FB.ActivateApp();
+                });
+            }
+        }
+    }
+}
